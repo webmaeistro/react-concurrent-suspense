53284ce5738fcdef2915a88c91f7b970266c5e33
Martin Andersen <marander@protonmail.com>
2020-04-16 20:03:50 +0200

    suspense printout added

--- src/index.js
+++ src/index.js
@@ -9,11 +9,11 @@ const resource = fetchProfileData();
 function ProfilePage() {
   return (
     <Suspense
-      fallback={<h1>Loading profile...</h1>}
+      fallback={<h1>suspense-Loading profile...</h1>}
     >
       <ProfileDetails />
       <Suspense
-        fallback={<h1>Loading posts...</h1>}
+        fallback={<h1>suspense-Loading posts...</h1>}
       >
         <ProfileTimeline />
       </Suspense>
